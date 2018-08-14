910532ebcbe4dd715ced5efba9c9c1300801e896
MarceloRezin <marcelo.rezin@gmail.com>
2018-08-13 21:50:21 -0300

    Modelagem do token

---
+++ src/token/Token.java
@@ -0,0 +1,35 @@
+package token;
+
+import enuns.Codigo;
+
+public class Token {
+
+    private String palavra;
+    private Codigo codigo;
+
+    public Token(String palavra) {
+        this.codigo = Codigo.valueOfByPalavra(palavra);
+        this.palavra = palavra;
+    }
+
+    public Token(String palavra, Codigo codigo) {
+        this.palavra = palavra;
+        this.codigo = codigo;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public Codigo getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Codigo codigo) {
+        this.codigo = codigo;
+    }
+}

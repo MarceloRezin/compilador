c596ac81f5d65a4f2d0a6911ca052d549a7b7a6c
Marcelo Rezin <marcelo.rezin@gmai.com>
2018-08-19 18:58:34 -0300

    Representa os tipos de entrada

---
+++ src/enuns/TipoEntrada.java
@@ -0,0 +1,37 @@
+package enuns;
+
+import utils.CharUtils;
+
+public enum TipoEntrada {
+
+    ESPACO,
+    QUALQUER,
+    LETRA,
+    FIM,
+    DIGITO,
+    OPERADOR;
+
+    public static TipoEntrada valueOfByCaracter(Integer caracter){
+        if(caracter == -1){
+            return FIM;
+        }
+
+        if(CharUtils.isEspaco(caracter)){
+            return ESPACO;
+        }
+
+        if(CharUtils.isLetra(caracter)){
+            return LETRA;
+        }
+
+        if(CharUtils.isDigito(caracter)){
+            return DIGITO;
+        }
+
+        if(CharUtils.isOperador(caracter)){
+            return OPERADOR;
+        }
+
+        return null;
+    }
+}

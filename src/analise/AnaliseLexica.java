bc8270b9c77225857e12e9f31b5914f77b90a362
Marcelo Rezin <marcelo.rezin@gmai.com>
2018-08-19 19:02:07 -0300

    Melhora do automato

--- src/analise/AnaliseLexica.java
+++ src/analise/AnaliseLexica.java
@@ -3,7 +3,8 @@ package analise;
 import arquivo.Leitor;
 import automatos.Automato;
 import automatos.Estado;
-import enuns.Tipo;
+import enuns.TipoEntrada;
+import enuns.TipoRetorno;
 import exceptions.AnaliseLexicaException;
 import token.Token;
 
@@ -23,7 +24,9 @@ public class AnaliseLexica {
         while (leitor.getCaracterLido() > 0){
             Token token = automato.executar(leitor);
             if(token != null){
-                tokens.push(token);
+                if(!token.isIgnorado()){
+                    tokens.push(token);
+                }
             }else{
                 leitor.lerProximo();
             }
@@ -42,81 +45,72 @@ public class AnaliseLexica {
         return tokens;
     }
 
-    /*
-    Possibilidades:
-        CARACTER
-        OPERADOR
-        ESPACO
-        DIGITO
-        FIM
-        LITERAL
-     */
     private static Automato getAutomato(){
 
         Estado e1 = new Estado();
 
         //Identifica espacos
-        Estado e14 = new Estado(Tipo.ESPACO);
-        e1.addTransicao(Tipo.ESPACO, e14);
+        Estado e2 = new Estado();
+        Estado e3 = new Estado(TipoRetorno.IGNORAR);
+        e1.addTransicao(TipoEntrada.ESPACO, e2);
+        e2.addTransicao(TipoEntrada.QUALQUER, e3);
 
         //Identificacao de caracteres e palavras chave
-        Estado e2 = new Estado();
-        Estado e3 = new Estado();
         Estado e4 = new Estado();
-        Estado e5 = new Estado(Tipo.IDENTIFICADOR);
+        Estado e5 = new Estado();
+        Estado e6 = new Estado();
+        Estado e7 = new Estado(TipoRetorno.IDENTIFICADOR);
 
-        e1.addTransicao(Tipo.CARACTER, e2);
-        e2.addTransicao(Tipo.CARACTER, e2);
-        e2.addTransicao(Tipo.OPERADOR, e3);
-        e2.addTransicao(Tipo.ESPACO, e3);
-        e2.addTransicao(Tipo.FIM, e3);
-        e2.addTransicao(Tipo.DIGITO, e4);
-        e4.addTransicao(Tipo.DIGITO, e4);
-        e4.addTransicao(Tipo.CARACTER, e4);
-        e4.addTransicao(Tipo.OPERADOR, e5);
-        e4.addTransicao(Tipo.ESPACO, e5);
-        e4.addTransicao(Tipo.FIM, e5);
+        e1.addTransicao(TipoEntrada.LETRA, e4);
+        e4.addTransicao(TipoEntrada.LETRA, e4);
+        e4.setExcecao(e5);
+        e4.addTransicao(TipoEntrada.DIGITO, e6);
+        e6.addTransicao(TipoEntrada.DIGITO, e6);
+        e6.addTransicao(TipoEntrada.LETRA, e6);
+        e6.setExcecao(e7);
 
         //Identificacao de inteiros
-        Estado e6 = new Estado();
-        Estado e7 = new Estado(Tipo.DIGITO);
-        Estado e8 = new Estado(Tipo.ERRO);
+        Estado e8 = new Estado();
+        Estado e9 = new Estado(TipoRetorno.INTEIRO);
+        Estado e10 = new Estado(TipoRetorno.ERRO);
 
-        e1.addTransicao(Tipo.DIGITO, e6);
-        e6.addTransicao(Tipo.DIGITO, e6);
-        e6.addTransicao(Tipo.OPERADOR, e7);
-        e6.addTransicao(Tipo.ESPACO, e7);
-        e6.addTransicao(Tipo.FIM, e7);
-        e6.addTransicao(Tipo.CARACTER, e8);
+        e1.addTransicao(TipoEntrada.DIGITO, e8);
+        e8.addTransicao(TipoEntrada.DIGITO, e8);
+        e8.setExcecao(e9);
+        e8.addTransicao(TipoEntrada.LETRA, e10);
 
         //Identificacao de operadores, literais e comentarios
-        Estado e9 = new Estado();
-        Estado e10 = new Estado();
-        Estado e11 = new Estado(Tipo.LITERAL);
-        Estado e12 = new Estado();
-        Estado e13 = new Estado(Tipo.OPERADOR);
+        Estado e11 = new Estado();
+        Estado e12 = new Estado(TipoRetorno.OPERADOR);
+        Estado e13 = new Estado();
+        Estado e14 = new Estado();
+        Estado e15 = new Estado(TipoRetorno.LITERAL);
+//
+        e1.addTransicao(TipoEntrada.OPERADOR, e11);
+        e11.addTransicao(TipoEntrada.QUALQUER, e12);
+//        e11.setExcecao(e12);
+        e1.addTransicao("'", e13);
+        e13.addTransicao("'",   e14);
+        e13.addTransicao(TipoEntrada.LETRA, e13);
+        e13.addTransicao(TipoEntrada.DIGITO, e13);
+        e13.addTransicao(TipoEntrada.ESPACO, e13);
+        e14.addTransicao(TipoEntrada.QUALQUER, e15);
+//        e9.addTransicao(Tipo.ESPACO, e9);
+//        e9.addTransicao(Tipo.LITERAL, e10);
+//        e10.addTransicao(Tipo.LITERAL, e11);
+//        e10.addTransicao(Tipo.OPERADOR, e11);
+//        e10.addTransicao(Tipo.CARACTER, e11);
+//        e10.addTransicao(Tipo.DIGITO, e11);
+//        e10.addTransicao(Tipo.ESPACO, e11);
+//        e10.addTransicao(Tipo.FIM, e11);
+//
+//        e12.addTransicao(Tipo.LITERAL, e13);
+//        e12.addTransicao(Tipo.OPERADOR, e13);
+//        e12.addTransicao(Tipo.CARACTER, e13);
+//        e12.addTransicao(Tipo.DIGITO, e13);
+//        e12.addTransicao(Tipo.ESPACO, e13);
+//        e12.addTransicao(Tipo.FIM, e13);
 
-        e1.addTransicao(Tipo.LITERAL, e9);
-        e1.addTransicao(Tipo.OPERADOR, e12);
-        e9.addTransicao(Tipo.CARACTER, e9);
-        e9.addTransicao(Tipo.DIGITO, e9);
-        e9.addTransicao(Tipo.ESPACO, e9);
-        e9.addTransicao(Tipo.OPERADOR, e9);
-        e9.addTransicao(Tipo.LITERAL, e10);
-        e10.addTransicao(Tipo.LITERAL, e11);
-        e10.addTransicao(Tipo.OPERADOR, e11);
-        e10.addTransicao(Tipo.CARACTER, e11);
-        e10.addTransicao(Tipo.DIGITO, e11);
-        e10.addTransicao(Tipo.ESPACO, e11);
-        e10.addTransicao(Tipo.FIM, e11);
-
-        e12.addTransicao(Tipo.LITERAL, e13);
-        e12.addTransicao(Tipo.OPERADOR, e13);
-        e12.addTransicao(Tipo.CARACTER, e13);
-        e12.addTransicao(Tipo.DIGITO, e13);
-        e12.addTransicao(Tipo.ESPACO, e13);
-        e12.addTransicao(Tipo.FIM, e13);
-
-       return new Automato(e1, e3, e5, e7, e8, e11, e13, e14);
+       return new Automato(e1, e3, e5, e7, e9, e10, e12, e15);
     }
 }

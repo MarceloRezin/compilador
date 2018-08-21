4256ba89bc2bf18b43e48f401bab94e422e91e5e
Marcelo Rezin <marcelo.rezin@gmai.com>
2018-08-20 21:10:18 -0300

    main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,8 @@
 package main;
 
+import analise.AnaliseLexica;
 import arquivo.Arquivo;
-import automatos.Automato;
-import automatos.Estado;
-import enuns.Tipo;
+import exceptions.AnaliseLexicaException;
 
 import java.io.IOException;
 
@@ -18,27 +17,10 @@ public class Main {
 //        }
 
 
-        Estado e1 = new Estado();
-        Estado e2 = new Estado();
-        Estado e3 = new Estado();
-        Estado e4 = new Estado();
-        Estado e5 = new Estado(Tipo.IDENTIFICADOR);
-
-        e1.addTransicao(Tipo.CARACTER, e2);
-        e2.addTransicao(Tipo.CARACTER, e2);
-        e2.addTransicao(Tipo.OPERADOR, e3);
-        e2.addTransicao(Tipo.ESPACO, e3);
-        e2.addTransicao(Tipo.FIM, e3);
-        e2.addTransicao(Tipo.DIGITO, e4);
-        e4.addTransicao(Tipo.DIGITO, e4);
-        e4.addTransicao(Tipo.CARACTER, e4);
-        e4.addTransicao(Tipo.OPERADOR, e5);
-        e4.addTransicao(Tipo.ESPACO, e5);
-        e4.addTransicao(Tipo.FIM, e5);
-
-
-        Automato automato = new Automato(e1, e3, e5);
-
-        System.out.println(automato.executar(Arquivo.ler("texto.txt")));
+        try {
+            AnaliseLexica.analisar(Arquivo.ler("texto.txt"));
+        } catch (AnaliseLexicaException e) {
+            e.printStackTrace();
+        }
     }
 }

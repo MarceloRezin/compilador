cf333e2cd088925258a326010cd8949966081fc1
Marcelo Rezin <marcelo.rezin@gmai.com>
2018-09-10 20:27:35 -0300

    Códigos não terminais

--- src/enuns/Codigo.java
+++ src/enuns/Codigo.java
@@ -36,6 +36,9 @@ public enum Codigo {
     FOR(27),
     TO(28),
     CASE(29),
+    LITERAL(48),
+
+    //Simbolos especiais
     OP_SOMA(30, "+"),
     OP_SUB(31, "-"),
     OP_MULT(32, "*"),
@@ -54,10 +57,51 @@ public enum Codigo {
     OP_DIFERENTE(45, "<>"),
     OP_VIRGULA(46, ","),
     OP_PONTO_VIRGULA(47, ";"),
-    LITERAL(48),
     OP_PONTO(49, "."),
     OP_PONTO_PONTO(50, ".."),
-    OP_CIFRAO(51, "$");
+    OP_CIFRAO(51, "$"),
+
+
+    /*
+    NAO TERMINAIS
+     */
+    PROGRAMA(52),
+    BLOCO(53),
+    DCLROT(54),
+    LID(55),
+    REPIDENT(56),
+    DCLCONST(57),
+    LDCONST(58),
+    DCLVAR(59),
+    LDVAR(60),
+    TIPO(61),
+    DCLPROC(62),
+    DEFPAR(63),
+    CORPO(64),
+    REPCOMANDO(65),
+    COMANDO(66),
+    RCOMID(67),
+    RVAR(68),
+    PARAMETROS(69),
+    REPPAR(70),
+    ELSEPARTE(71),
+    VARIAVEL(72),
+    VARIAVEL1(73),
+    REPVARIAVEL(74),
+    ITEMSAIDA(75),
+    REPITEM(76),
+    EXPRESSAO(77),
+    REPEXPSIMP(78),
+    EXPSIMP(79),
+    REPEXP(80),
+    TERMO(81),
+    REPTERMO(82),
+    FATOR(83),
+    CONDCASE(84),
+    CONTCASE(85),
+    RPINTEIRO(86),
+    SEMEFEITO(87);
+
 
     private int codigo;
     String caracter;
